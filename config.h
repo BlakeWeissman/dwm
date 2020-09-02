4d375bef058f8a5f4d8d1bf7b6cf869ce1f8b4f1
Blake Weissman <contact@blakeweissman.com>
2020-09-02 17:40:08 -0400

    Changed screenshot directory for syncthing purposes

--- config.h
+++ config.h
@@ -108,7 +108,7 @@ static Key keys[] = {
 	{ MODKEY,												XK_space,	zoom,		{0} },
 	{ MODKEY,												XK_f,  cyclelayout,    {.i = -1 } },
 	//TODO: Bind key to xkeys?
-	{ 0,														XK_Print,	spawn,	SHCMD("maim screenshots/pic-full-$(date '+%y%m%d-%H%M-%S').png") },
+	{ 0,														XK_Print,	spawn,	SHCMD("maim Screenshots/pic-full-$(date '+%y%m%d-%H%M-%S').png") },
 };
 
 /* button definitions */
@@ -126,5 +126,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	// TODO: Implement keybinding for this
 	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
 };
